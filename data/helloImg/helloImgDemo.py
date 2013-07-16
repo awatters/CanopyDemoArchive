22a7d6f3e9ca0947da4f9720b24656011e67cec1
Aaron Watters <awatters@enthought.com>
2013-07-16 16:56:12 -0400

    Use stuff in the zip files.

--- data/helloImg/helloImgDemo.py
+++ data/helloImg/helloImgDemo.py
@@ -1,1 +1,29 @@
 print "hello world"
+from skimpyGimpy import canvas
+c = canvas.Canvas()
+
+# set the foreground color to a light blue
+c.setColor(99,99,0xff)
+
+# set the background color to a dark green
+c.setBackgroundColor(10,50,0)
+
+# name a font "propell" and associate it with a BDF font file
+c.addFont("propell", "fonts/mlmfonts/propell.bdf")
+
+# set the current font to "propell" with scale 2.0 and
+# pixel radius 1.3
+c.setFont("propell", 2.0, 1.3)
+
+# draw some text
+c.addText(0,0, "Hello PNG World")
+
+c.dumpToPNG("HelloWorld.png")
+
+import matplotlib.image as mpimg
+
+img=mpimg.imread('HelloWorld.png')
+
+import matplotlib.pyplot as plt
+imgplot = plt.imshow(img)
+plt.show()
